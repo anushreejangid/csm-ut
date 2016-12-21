ca8cbda1d21b96bca37d198062b35d4a6c2ce824
Kamal R. Prasad <kamalpr@cisco.com>
2016-12-21 15:54:02 +0530

    sync up

---
+++ csmpe/core_plugins/csm_custom_commands_capture/plugin.py
@@ -0,0 +1,67 @@
+# =============================================================================
+#
+# Copyright (c) 2016, Cisco Systems
+# All rights reserved.
+#
+# # Author: Klaudiusz Staniek
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
+
+from csmpe.plugins import CSMPlugin
+from condoor.exceptions import CommandSyntaxError
+from csmpe.core_plugins.csm_install_operations.exr.install import send_admin_cmd, match_pattern
+from csmpe.core_plugins.csm_install_operations.exr.install import report_log
+import re
+
+class Plugin(CSMPlugin):
+    """This plugin captures custom commands and stores in the log directory."""
+    name = "Custom Commands Capture Plugin"
+    platforms = {'ASR9K', 'CRS', 'NCS1K', 'NCS5K', 'NCS5500', 'NCS6K', 'ASR900', 'N6K', 'IOS-XRv'}
+    phases = {'Pre-Upgrade', 'Post-Upgrade'}
+
+    def run(self):
+        command_list = self.ctx.custom_commands
+        print self.ctx.custom_commands
+        shell = self.ctx.shell
+        if command_list:
+            for cmd in command_list:
+                self.ctx.info("Capturing output of '{}' for shell {}".format(cmd,shell))
+                try:
+                    if shell == "SysadminBash" or shell == "Sysadmin":
+                        self.ctx.info("Sending admin command")
+                        output = send_admin_cmd(self.ctx, cmd)
+                    else:
+                        output = self.ctx.send(cmd, timeout=2200)
+                    self.ctx.info("command sent {}".format(cmd))
+                    if self.ctx.pattern:
+                        status, message = match_pattern(self.ctx.pattern, output) 
+                        report_log(self.ctx, status, message)
+                    file_name = self.ctx.save_to_file(cmd, output)
+                    if file_name is None:
+                        self.ctx.error("Unable to save '{}' output to file: {}".format(cmd, file_name))
+                        return False
+                except CommandSyntaxError:
+                    self.ctx.error("Command Syntax Error: '" + cmd + "'")
+
+        else:
+            self.ctx.info("No custom commands provided.")
+            return True
